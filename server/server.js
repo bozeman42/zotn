5ad14cb440b5f1b2fdb4edb4212f687d521e5508
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2018-02-26 20:03:03 -0600

    rearranged some things, added comments

--- server/server.js
+++ server/server.js
@@ -5,10 +5,9 @@ const path = require('path');
 const playerRouter = require('./routes/router-player');
 
 const app = express();
-
 const PORT = 5000;
 
-app.use('/players',playerRouter);
 app.use(express.static(`${__dirname}/../build/`));
+app.use('/players',playerRouter);
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}...`))
--- src/scripts/controllers/controller-player-select.js
+++ src/scripts/controllers/controller-player-select.js
@@ -1,3 +1,6 @@
+// injected dependencies:
+// PlayerService
+
 export default class PlayerSelectController {
   constructor(PlayerService) {
     this.data = PlayerService.data;
