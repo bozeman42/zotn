50e9c5178cb9b13368ba8cd151bc517f67d02887
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2018-03-17 00:08:22 -0500

    routes for registering assets created

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 // routes
 const playerRouter = require('./routes/router-player');
 const factionRouter = require('./routes/router-faction');
+const assetRouter = require('./routes/router-asset');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,5 +16,6 @@ app.use(bodyParser.json());
 
 app.use('/players',playerRouter);
 app.use('/faction',factionRouter);
+app.use('/assets',factionRouter);
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}...`))
